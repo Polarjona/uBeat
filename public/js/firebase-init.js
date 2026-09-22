0ff5c1b0e37cbeafe7e3f0eb7d32479eaccb0355
Alejandro ENDRIZZI-ROMAN <alejandro.endrizzi.ext@inetum.com>
2026-09-22 08:11:08 +0200

    Palabra de Servidor Prohibida

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -3,9 +3,9 @@
 // las reglas las aplica Firebase + nuestro backend verifica el token).
 var firebaseConfig = {
   apiKey: "AIzaSyDNLzGiLPo7_u6yxi9ifqAnMDOVsxBG_IE",
-  authDomain: "proyecto-inetum-1.firebaseapp.com",
-  projectId: "proyecto-inetum-1",
-  storageBucket: "proyecto-inetum-1.firebasestorage.app",
+  authDomain: "proyecto-1.firebaseapp.com",
+  projectId: "proyecto-1",
+  storageBucket: "proyecto-1.firebasestorage.app",
   messagingSenderId: "362256147155",
   appId: "1:362256147155:web:97bead8396f151a334aa26",
   measurementId: "G-62GJ5DKS3H"
