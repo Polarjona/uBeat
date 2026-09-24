1a16aa58d7e135f5ccbf5504481cf029dc68e010
alejandrozz-1 <alejandrozz2007@gmail.com>
2026-09-24 11:57:32 +0200

    server cambios

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -2,12 +2,12 @@
 // La apiKey es pública por diseño (solo identifica el proyecto;
 // las reglas las aplica Firebase + nuestro backend verifica el token).
 var firebaseConfig = {
-  apiKey: "AIzaSyBaqmO-S7XmvXxmlKY_8rc-NDJeJbMYMXg",
-  authDomain: "ubeat-genai.firebaseapp.com",
-  projectId: "ubeat-genai",
-  storageBucket: "ubeat-genai.firebasestorage.app",
-  messagingSenderId: "1037066735481",
-  appId: "1:1037066735481:web:497c9f26f4c2635b1069a8"
+  apiKey: "AIzaSyDbG0V_ocYfEgVEEYEqZ-vaYJt4fnQ00vo",
+  authDomain: "ubeat-6ac17.firebaseapp.com",
+  projectId: "ubeat-6ac17",
+  storageBucket: "ubeat-6ac17.firebasestorage.app",
+  messagingSenderId: "211846950649",
+  appId: "1:211846950649:web:93f67593fe23467bb1fc28"
 };
 
 firebase.initializeApp(firebaseConfig);
